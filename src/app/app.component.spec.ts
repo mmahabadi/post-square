67ffbc89aecee98896e4591ec301a2468abef7f5
Mohsen <m.mehabadi@gmail.com>
2024-05-10 18:03:10 +0200

    add loading component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,82 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { loadPosts } from './posts/+state/post.actions';
+import { selectError, selectLoading } from './posts/+state/post.selectors';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore;
+  const initialState = {};
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
+    store = TestBed.inject(MockStore);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'post-square' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('post-square');
-  });
+  it('should render the header, posts, and selected post components', function () {
+    // Arrange
+    store.overrideSelector(selectLoading, false);
+    store.overrideSelector(selectError, null);
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // Act
+    store.refreshState();
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, post-square');
+    // Assert
+    expect(fixture.nativeElement.querySelector('app-posts')).toBeTruthy();
+    expect(
+      fixture.nativeElement.querySelector('app-selected-post')
+    ).toBeTruthy();
+    expect(fixture.nativeElement.querySelector('app-loading')).toBeFalsy();
+    expect(fixture.nativeElement.querySelector('app-alert')).toBeFalsy();
+  });
+
+  it('should dispatch loadPosts action on init', function () {
+    // Arrange
+    const loadPostsSpy = spyOn(store, 'dispatch').and.callThrough();
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(loadPostsSpy).toHaveBeenCalledOnceWith(loadPosts());
+  });
+
+  it('should render loading component when loading$ is true', () => {
+    // Arrange
+    store.overrideSelector(selectLoading, true);
+
+    //Act
+    store.refreshState();
+    fixture.detectChanges();
+
+    // Assert
+    const loadingComponent = fixture.nativeElement.querySelector('app-loading');
+    expect(loadingComponent).toBeTruthy();
+  });
+
+  it('should render error alert when error$ has a value', () => {
+    // Arrange
+    const errorMessage = new Error('An error occurred');
+    store.overrideSelector(selectError, errorMessage);
+
+    //Act
+    store.refreshState();
+    fixture.detectChanges();
+
+    // Assert
+    const errorAlert = fixture.nativeElement.querySelector('app-alert');
+    expect(errorAlert).toBeTruthy();
+    expect(errorAlert.textContent).toContain(errorMessage);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,42 @@
 import { AsyncPipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AlertComponent } from '@share/components/alert.component';
+import { HeaderComponent } from '@share/components/header.component';
+import { LoadingComponent } from '@share/components/loading.component';
 import { loadPosts } from './posts/+state/post.actions';
 import { selectError, selectLoading } from './posts/+state/post.selectors';
 import { PostsComponent } from './posts/posts.component';
+import { SelectedPostComponent } from './posts/selected-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HttpClientModule, AsyncPipe, PostsComponent, AlertComponent],
-  template: ` @if (loading$ | async) {
-    <div>Loading...</div>
-    } @if (error$ | async) {
-    <app-alert class="text-center">{{ error$ | async }}</app-alert>
-    }
-
-    <app-posts></app-posts>`,
+  imports: [
+    AsyncPipe,
+    PostsComponent,
+    AlertComponent,
+    HeaderComponent,
+    LoadingComponent,
+    SelectedPostComponent,
+  ],
+  template: `
+    <app-selected-post />
+    <main class="pt-16">
+      @if (loading$ | async) {
+      <app-loading />
+      } @if (error$ | async) {
+      <app-alert class="text-center">{{ error$ | async }}</app-alert>
+      }
+      <app-posts></app-posts>
+    </main>
+  `,
 })
 export class AppComponent {
   private readonly store = inject(Store);
   loading$ = this.store.select(selectLoading);
   error$ = this.store.select(selectError);
+
   ngOnInit(): void {
     this.store.dispatch(loadPosts());
   }
